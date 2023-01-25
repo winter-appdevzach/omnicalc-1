79c1f45e59fe74926dfe691d92d9af01a0ef6726
Appdevzach <zmarciano@uchicago.edu>
2023-01-25 21:32:34 +0000

    done with random and square

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,26 @@ class ApplicationController < ActionController::Base
     render ({:template => "calculation_templates/square_form.html.erb"})
   end
 
-  def caculate_square 
+  def calculate_square 
     @num = params.fetch("elephant").to_f
     @square_of_num = @num * @num
     render ({:template => "calculation_templates/square_results.html.erb"})
   end
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render ({:template => "calculation_templates/rand_results.html.erb"})
+  end
+
+  def blank_random_form 
+
+    render ({:template => "calculation_templates/random_form.html.erb"})
+
+  end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
 get("/square/new", {:controller =>"application", :action => "blank_square_form"})
 
-get("/square/results", {:controller => "application", :action => "caculate_square"})
+get("/square/results", {:controller => "application", :action => "calculate_square"})
+
+get("/random/results", {:controller => "application", :action => "calculate_random"})
+
+get("/random/new", {:controller => "application", :action => "blank_random_form"})
+
+
 
 
 end
